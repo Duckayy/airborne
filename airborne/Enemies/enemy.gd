f2c24223b1d3fff2251b4b347c6a26b60e06cfdc
noob18211 <nnn.noob1821@gmail.com>
2026-06-03 00:53:27 -0700

    Update enemy.gd

--- airborne/Enemies/enemy.gd
+++ airborne/Enemies/enemy.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-<<<<<<< Updated upstream
+
 @export var max_health = 100.0
 @export var move_speed = 6.0
 @export var attack_range = 2.0
@@ -86,31 +86,3 @@ func _die():
 	queue_free()
 	
 	
-=======
-
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
->>>>>>> Stashed changes
