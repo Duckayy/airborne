a0da6e817d66a81cd83277e630bd75987fb89fbc
duckayy <legaspiashton@gmail.com>
2026-07-21 22:28:45 -0700

    done

--- airborne/Models/Viewmodels/Script/ViewModels.gd
+++ airborne/Models/Viewmodels/Script/ViewModels.gd
@@ -5,9 +5,10 @@ var item_data = JsonData.LoadData("res://Data/ItemData.json")
 var item_asset_data = JsonData.LoadData("res://Data/ItemAssetData.json")
 
 @onready var player = $"../../.."
-@onready var model = $SubViewportContainer/SubViewport/Sprite3D
-@onready var animation = $SubViewportContainer/SubViewport/Sprite3D/AnimationPlayer
+@onready var model = $SubViewportContainer/SubViewport/Swords
+@onready var animation = $SubViewportContainer/SubViewport/Swords/AnimationPlayer
 
+var current_instance: Node3D = null
 var lib := AnimationLibrary.new()
 var item_resources = {}
 var item_animation = {}
@@ -27,20 +28,22 @@ func load_item_models():
 func load_item_animations():
 	for item in item_asset_data:
 		var ani = load(item_asset_data[item]["UseAnimation"])
-		lib.add_animation("UseAnimation", ani)
+		lib.add_animation(item, ani)
 	animation.add_animation_library("UseAnimation", lib)
 	
 
 func load_model(model_id):
+	if current_instance != null:
+		current_instance.queue_free()
+		current_instance = null
 	if model_id != null:
-		model.texture = item_resources[model_id]
-	else:
-		model.texture = null
+		var scene: PackedScene = item_resources[model_id]
+		current_instance = scene.instantiate()
+		model.add_child(current_instance)
 	current_item = model_id
-	print(model.texture)
 	
 func play_animation():
 	#animation.play(item_asset_data[]["UseAnimation"])
 	if current_item != null:
-		animation.play("prototype_sword")
+		animation.play("UseAnimation/" + current_item)
 	pass
