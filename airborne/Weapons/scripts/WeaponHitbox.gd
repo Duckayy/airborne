cbafd32a649fea4d1c454dea42954089aa09d9a1
duckayy <legaspiashton@gmail.com>
2026-08-31 00:32:55 -0700

    Hitbox
    -CHanges
    Changed hitbox to not form around sword but works independently with sword just being the animation that indicates swinging. Its a box

--- airborne/Weapons/scripts/WeaponHitbox.gd
+++ airborne/Weapons/scripts/WeaponHitbox.gd
@@ -3,15 +3,11 @@ extends Node
 @export var damage: int
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Swords/Area3D.body_entered.connect(_on_body_entered)
+	$Area3D.body_entered.connect(_on_body_entered)
 	pass # Replace with function body.
 
 func setup(data) -> void:
 	damage = data["Damage"]
-	var mesh_scene: PackedScene = load(data["ModelPath"])
-	var mesh_instance = mesh_scene.instantiate()
-	$Swords.add_child(mesh_instance)
-	
 
 func _on_body_entered(body: Node3D) -> void:
 	print("hit: ", body.name)
